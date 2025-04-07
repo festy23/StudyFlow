5aa437f5f17a7728e364b25a69625a22ddc41ae2
Kandler3 <artchuk@icloud.com>
2025-04-08 02:10:33 +0300

    explicit optional fields

--- user_service/pkg/api/user_service_grpc.pb.go
+++ user_service/pkg/api/user_service_grpc.pb.go
@@ -278,7 +278,7 @@ func (UnimplementedUserServiceServer) testEmbeddedByValue()                     
 
 // UnsafeUserServiceServer may be embedded to opt out of forward compatibility for this service.
 // Use of this interface is not recommended, as added methods to UserServiceServer will
-// result in compilation errdefs.
+// result in compilation errors.
 type UnsafeUserServiceServer interface {
 	mustEmbedUnimplementedUserServiceServer()
 }
